02ef9afda49413c12753f214dcf24f24fe70c16c
grayghostvisuals <grayghost@grayghostvisuals.com>
2014-02-08 19:03:49 -0500

    Splits Scripts. Adds Patterns to Snippets Dir

--- js/script.js
+++ js/script.js
@@ -1,50 +1,3 @@
-// Also see http://youmightnotneedjquery.com for some helpful bits and bobs.
-
-// JS Patterns
-// ===========================================
-// http://toddmotto.com/avoiding-anonymous-javascript-functions
-// http://benalman.com/news/2010/11/immediately-invoked-function-expression
-// http://toddmotto.com/stop-toggling-classes-with-js-use-behaviour-driven-dom-manipulation-with-data-states
-
-// IIFE
-// keep things outside the global scope plz
-(function (window, document, undefined) {
-	'use strict';
-})(window, document);
-
-
-// Function Declaration
-function foo(){ /* code */ }
-
-
-// Object Function Method
-// http://base.webknit.co.uk
-var Base = Base || {};
-
-Base.functionName = function() {
-	var self = $(this),
-		variable = $('element');
-
-	function init() {
-		variable.click(functionOne);
-		functionTwo();
-	}
-
-	function functionOne() { /*code*/ }
-
-	function functionTwo() {
-		window.hide();
-	}
-
-	init();
-};
-
-// On Doc Ready
-$(function() {
-	new Base.functionName();
-});
-
-
 // Disable Hover
 // thecssninja.com/javascript/pointer-events-60fps
 // ===========================================
@@ -63,24 +16,6 @@ window.addEventListener('scroll', function() {
 		body.classList.remove('disable-hover');
 	},500);
 }, false);
-
-
-// Placeholder Polyfill
-// github.com/mathiasbynens/jquery-placeholder
-// ===========================================
-
-$('input, textarea').placeholder();
-
-
-// Matchmedia Polyfill
-// github.com/paulirish/matchMedia.js
-// ===========================================
-
-// Width Detection
-if (matchMedia('only screen and (max-width: 480px)').matches) {}
-
-// Orientation Detection
-if (matchMedia('all and (orientation:landscape)').matches) {}
 
 
 // SVG Magic
@@ -109,3 +44,21 @@ $('img[src*=".svgz"]').svgmagic({
 	callback: false, // Function to run after images are changed, default: false
 	dumpcache: true // Force to remove the cache and create a new .PNg, default: false
 });
+
+
+// Placeholder Polyfill
+// github.com/mathiasbynens/jquery-placeholder
+// ===========================================
+
+$('input, textarea').placeholder();
+
+
+// Matchmedia Polyfill
+// github.com/paulirish/matchMedia.js
+// ===========================================
+
+// Width Detection
+if (matchMedia('only screen and (max-width: 480px)').matches) {}
+
+// Orientation Detection
+if (matchMedia('all and (orientation:landscape)').matches) {}
